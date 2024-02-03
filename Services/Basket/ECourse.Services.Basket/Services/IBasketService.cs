4496b27375886d4d4423c3d284f1c7c029016e25
Mutlu Özkurt <mutluozkurt@gmail.com>
2024-02-03 18:48:15 +0300

    basket service completed

---
+++ Services/Basket/ECourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using ECourse.Services.Basket.Dtos;
+using ECourse.Shared.Dtos;
+using System.Threading.Tasks;
+
+namespace ECourse.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
---
+++ Shared/ECourse.Shared/Services/SharedIdentityService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECourse.Shared.Services
+{
+    public class SharedIdentityService : ISharedIdentityService
+    {
+        private IHttpContextAccessor _htttpContextAccessor;
+
+        public SharedIdentityService(IHttpContextAccessor htttpContextAccessor)
+        {
+            _htttpContextAccessor = htttpContextAccessor;
+        }
+
+        public string GetUserId => _htttpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
